5c5a301ed5933656c7d79ae162efab7137af9d01
Kurt Paris <kurtpa@phoenixnap.com>
2016-07-26 10:43:06 +0200

    Updating Version to 0.8.1 Added ASF license to new files Added Javadoc & @since annotations

--- springmvc-raml-parser/src/main/java/com/phoenixnap/oss/ramlapisync/raml/rjp/raml08v1/RJP08V1RamlModelEmitter.java
+++ springmvc-raml-parser/src/main/java/com/phoenixnap/oss/ramlapisync/raml/rjp/raml08v1/RJP08V1RamlModelEmitter.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.phoenixnap.oss.ramlapisync.raml.rjp.raml08v1;
 
 import com.phoenixnap.oss.ramlapisync.raml.RamlModelEmitter;
@@ -5,7 +17,10 @@ import com.phoenixnap.oss.ramlapisync.raml.RamlRoot;
 import org.raml.emitter.RamlEmitter;
 
 /**
+ * Implementation based on the Raml 0.8 Parser
+ * 
  * @author armin.weisser
+ * @since 0.8.1
  */
 public class RJP08V1RamlModelEmitter implements RamlModelEmitter {
 
