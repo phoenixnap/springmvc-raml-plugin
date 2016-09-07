38507b9aa7e027e1245aa922ed43a4b80ee03874
Leanne Borg <leanneb@ccbilleu.com>
2016-09-07 15:40:44 +0200

    - Added missing curly brackets for class definition

--- springmvc-raml-parser/src/main/java/com/phoenixnap/oss/ramlapisync/generation/rules/spring/SpringRestClientMethodBodyRule.java
+++ springmvc-raml-parser/src/main/java/com/phoenixnap/oss/ramlapisync/generation/rules/spring/SpringRestClientMethodBodyRule.java
@@ -48,8 +48,8 @@ import com.sun.codemodel.JType;
 import com.sun.codemodel.JVar;
 
 /**
- * Generates a method body that calls the rest template to execute a REST call. 
- * 
+ * Generates a method body that calls the rest template to execute a REST call.
+ *
  * INPUT:
  * #%RAML 0.8
  * title: myapi
@@ -65,19 +65,19 @@ import com.sun.codemodel.JVar;
  *
  * The name of the field can be configured. Default is "restTemplate".
  *
- * @author Kurt Paris 
+ * @author Kurt Paris
  * @author Kris Galea
  * @since 0.5.0
  */
 public class SpringRestClientMethodBodyRule implements Rule<CodeModelHelper.JExtMethod, JMethod, ApiActionMetadata> {
 
     private String restTemplateFieldName = "restTemplate";
-    
+
     private String baseUrlFieldName = "baseUrl";
-    
+
     public SpringRestClientMethodBodyRule(String restTemplateFieldName, String baseUrlFieldName) {
         if(StringUtils.hasText(restTemplateFieldName)) {
-            this.restTemplateFieldName = restTemplateFieldName;            
+            this.restTemplateFieldName = restTemplateFieldName;
         }
         if (!StringUtils.isEmpty(baseUrlFieldName)){
             this.baseUrlFieldName = baseUrlFieldName;
@@ -88,8 +88,8 @@ public class SpringRestClientMethodBodyRule implements Rule<CodeModelHelper.JExt
     public JMethod apply(ApiActionMetadata endpointMetadata, CodeModelHelper.JExtMethod generatableType) {
         JBlock body = generatableType.get().body();
         JCodeModel owner = generatableType.owner();
-        //build HttpHeaders   
-        JClass httpHeadersClass = owner.ref(HttpHeaders.class);        
+        //build HttpHeaders
+        JClass httpHeadersClass = owner.ref(HttpHeaders.class);
         JExpression headersInit = JExpr._new(httpHeadersClass);
         JVar httpHeaders = null;
         if (endpointMetadata.getInjectHttpHeadersParameter()) {
@@ -102,19 +102,19 @@ public class SpringRestClientMethodBodyRule implements Rule<CodeModelHelper.JExt
         } else {
             httpHeaders = body.decl(httpHeadersClass, "httpHeaders", headersInit);
         }
-        
-        
+
+
         //Declare Arraylist to contain the acceptable Media Types
         body.directStatement("//  Add Accepts Headers and Body Content-Type");
         JClass mediaTypeClass = owner.ref(MediaType.class);
         JClass refArrayListClass = owner.ref(ArrayList.class).narrow(mediaTypeClass);
-        JVar acceptsListVar = body.decl(refArrayListClass, "acceptsList", JExpr._new(refArrayListClass));        
+        JVar acceptsListVar = body.decl(refArrayListClass, "acceptsList", JExpr._new(refArrayListClass));
 
         //If we have a request body, lets set the content type of our request
         if (endpointMetadata.getRequestBody() != null) {
         	body.invoke(httpHeaders, "setContentType").arg(mediaTypeClass.staticInvoke("valueOf").arg(endpointMetadata.getRequestBodyMime()));
         }
-        
+
         //If we have response bodies defined, we need to add them to our accepts headers list
         //TODO possibly restrict
         String documentDefaultType = endpointMetadata.getParent().getDocument().getMediaType();
@@ -124,7 +124,7 @@ public class SpringRestClientMethodBodyRule implements Rule<CodeModelHelper.JExt
         } else { //default to application/json just in case
         	body.invoke(acceptsListVar, "add").arg(mediaTypeClass.staticInvoke("valueOf").arg("application/json"));
         }
-        
+
         //Iterate over Response Bodies and add each distinct mime type to accepts headers
         if (endpointMetadata.getResponseBody() != null && !endpointMetadata.getResponseBody().isEmpty()) {
         	for (String responseMime : endpointMetadata.getResponseBody().keySet()) {
@@ -133,13 +133,13 @@ public class SpringRestClientMethodBodyRule implements Rule<CodeModelHelper.JExt
         		}
         	}
         }
-       
+
         //Set accepts list as our accepts headers for the call
         body.invoke(httpHeaders, "setAccept").arg(acceptsListVar);
 
-        
+
         //Get the parameters from the model and put them in a map for easy lookup
-        List<JVar> params = generatableType.get().params();       
+        List<JVar> params = generatableType.get().params();
         Map<String, JVar> methodParamMap = new LinkedHashMap<>();
         for (JVar param : params) {
         	methodParamMap.put(param.name(), param);
@@ -152,73 +152,73 @@ public class SpringRestClientMethodBodyRule implements Rule<CodeModelHelper.JExt
             body._if(methodParamMap.get(javaParamName).ne(JExpr._null()))._then().block()
                     .invoke(httpHeaders, "add").arg(parameter.getName()).arg(JExpr.invoke(param, "toString"));
         }
-        
+
         //Build the Http Entity object
         JClass httpEntityClass = owner.ref(HttpEntity.class);
         JInvocation init = JExpr._new(httpEntityClass);
-        
+
         if (endpointMetadata.getRequestBody() != null) {
 	       init.arg(methodParamMap.get(CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, endpointMetadata.getRequestBody().getName())));
         }
         init.arg(httpHeaders);
-        
-        //Build the URL variable                
+
+        //Build the URL variable
         JExpression urlRef = JExpr.ref(baseUrlFieldName);
         JType urlClass = owner._ref(String.class);
-        JExpression targetUrl = urlRef.invoke("concat").arg(endpointMetadata.getResource().getUri());        
+        JExpression targetUrl = urlRef.invoke("concat").arg(endpointMetadata.getResource().getUri());
         JVar url = body.decl(urlClass, "url", targetUrl);
         JVar uriBuilderVar = null;
         JVar uriComponentVar = null;
-                
-       
-        
+
+
+
         //Initialise the UriComponentsBuilder
     	JClass builderClass = owner.ref(UriComponentsBuilder.class);
     	JExpression builderInit = builderClass.staticInvoke("fromHttpUrl").arg(url);
-        
+
         //If we have any Query Parameters, we will use the URIBuilder to encode them in the URL
         if (!CollectionUtils.isEmpty(endpointMetadata.getRequestParameters())) {
             //iterate over the parameters and add calls to .queryParam
             for (ApiParameterMetadata parameter : endpointMetadata.getRequestParameters()) {
             	builderInit = builderInit.invoke("queryParam").arg(parameter.getName()).arg(methodParamMap.get(NamingHelper.getParameterName(parameter.getName())));
-            }         
+            }
         }
         //Add these to the code model
         uriBuilderVar = body.decl(builderClass, "builder", builderInit);
-        
+
         JClass componentClass = owner.ref(UriComponents.class);
     	JExpression component = uriBuilderVar.invoke("build");
     	uriComponentVar = body.decl(componentClass, "uriComponents", component);
-        
-        //build request entity holder                
-        JVar httpEntityVar = body.decl(httpEntityClass, "httpEntity", init);        
-                        
-        //construct the HTTP Method enum 
+
+        //build request entity holder
+        JVar httpEntityVar = body.decl(httpEntityClass, "httpEntity", init);
+
+        //construct the HTTP Method enum
         JClass httpMethod = null;
         try {
             httpMethod = (JClass)owner._ref(HttpMethod.class);
         }
         catch (ClassCastException e) {
-            
-        } 
-       
-        //get all uri params from metadata set and add them to the param map in code 
+
+        }
+
+        //get all uri params from metadata set and add them to the param map in code
         if (!CollectionUtils.isEmpty(endpointMetadata.getPathVariables())) {
             //Create Map with Uri Path Variables
             JClass uriParamMap = owner.ref(Map.class).narrow(String.class, Object.class);
             JExpression uriParamMapInit = JExpr._new(owner.ref(HashMap.class));
             JVar uriParamMapVar = body.decl(uriParamMap, "uriParamMap", uriParamMapInit);
-            
+
         	endpointMetadata.getPathVariables().forEach(p -> body.invoke(uriParamMapVar, "put").arg(p.getName()).arg(methodParamMap.get(p.getName())));
         	JInvocation expandInvocation = uriComponentVar.invoke("expand").arg(uriParamMapVar);
 
-        	body.assign(uriComponentVar, expandInvocation);        	
+        	body.assign(uriComponentVar, expandInvocation);
         }
-        
-        //Determining response entity type 
+
+        //Determining response entity type
         JClass returnType = null;
         if (!endpointMetadata.getResponseBody().isEmpty()) {
-            ApiBodyMetadata apiBodyMetadata = endpointMetadata.getResponseBody().values().iterator().next();            
+            ApiBodyMetadata apiBodyMetadata = endpointMetadata.getResponseBody().values().iterator().next();
             JClass genericType = findFirstClassBySimpleName(apiBodyMetadata.getCodeModel(), apiBodyMetadata.getName());
             if (apiBodyMetadata.isArray()) {
                 JClass arrayType = owner.ref(List.class);
@@ -229,7 +229,7 @@ public class SpringRestClientMethodBodyRule implements Rule<CodeModelHelper.JExt
         } else {
             returnType = owner.ref(Object.class);
         }
-        
+
         JExpression returnExpression = JExpr.dotclass(returnType);//assume not parameterized by default
         //check if return is parameterized
         if (!CollectionUtils.isEmpty(returnType.getTypeParameters())) {
@@ -241,23 +241,23 @@ public class SpringRestClientMethodBodyRule implements Rule<CodeModelHelper.JExt
             JClass paramTypeRefClass = owner.ref(ParameterizedTypeReference.class);
             paramTypeRefClass = paramTypeRefClass.narrow(returnType);
 
-            body.directStatement("class _P extends " + paramTypeRefClass.fullName() + ";");
-            
+            body.directStatement("class _P extends " + paramTypeRefClass.fullName() + "{};");
+
             JExpression paramTypeRefInit = JExpr._new(owner.directClass("_P"));
             returnExpression = body.decl(paramTypeRefClass, "typeReference", paramTypeRefInit);
         }
-        
+
         //build rest template exchange invocation
         JInvocation jInvocation = JExpr._this().ref(restTemplateFieldName).invoke("exchange");
-        
+
         jInvocation.arg(uriComponentVar.invoke("encode").invoke("toUri"));
         jInvocation.arg(httpMethod.staticRef(endpointMetadata.getActionType().name()));
-        jInvocation.arg(httpEntityVar);         
+        jInvocation.arg(httpEntityVar);
         jInvocation.arg(returnExpression);
-        
+
         body._return(jInvocation);
 
         return generatableType.get();
     }
-  
+
 }
