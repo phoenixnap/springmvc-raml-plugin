787f4cea8f29366f85d65ecfe42e6df7c4edfe29
Rahul Kavale <rahul.kavale@vuclip.com>
2017-08-02 10:52:41 +0530

    Add throws exception when generating interfaces, WIP tests need to be fixed

--- springmvc-raml-parser/src/main/java/com/phoenixnap/oss/ramlapisync/generation/rules/basic/ControllerMethodSignatureRule.java
+++ springmvc-raml-parser/src/main/java/com/phoenixnap/oss/ramlapisync/generation/rules/basic/ControllerMethodSignatureRule.java
@@ -65,6 +65,7 @@ public class ControllerMethodSignatureRule implements Rule<JDefinedClass, JMetho
     public JMethod apply(ApiActionMetadata endpointMetadata, JDefinedClass generatableType) {
         JType responseType = responseTypeRule.apply(endpointMetadata, generatableType);
         JMethod jMethod = generatableType.method(JMod.PUBLIC, responseType, endpointMetadata.getName());
+        jMethod._throws(Exception.class);
         jMethod = paramsRule.apply(endpointMetadata, ext(jMethod, generatableType.owner()));
         return jMethod;
     }
