11d766ff05e0039bb163fe13e86d99e82c709b29
Yura Nosenko <yuranos87@gmail.com>
2017-10-31 19:14:50 +0200

    Shortcut mapping annotations for Spring 4

---
+++ springmvc-raml-parser/src/main/java/com/phoenixnap/oss/ramlapisync/generation/rules/spring/SpringShortcutMappingMethodAnnotationRule.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2002-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.phoenixnap.oss.ramlapisync.generation.rules.spring;
+
+import com.phoenixnap.oss.ramlapisync.data.ApiActionMetadata;
+import com.phoenixnap.oss.ramlapisync.generation.rules.Rule;
+import com.sun.codemodel.JAnnotationUse;
+import com.sun.codemodel.JMethod;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import static jdk.nashorn.internal.runtime.PropertyDescriptor.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+
+/**
+ * Adds one of following: {@literal @}GetMappping, {@literal @}PostMappping, {@literal @}PutMappping, {@literal @}DeleteMappping annotation at method level.
+ * <p>
+ * INPUT:
+ * #%RAML 0.8
+ * title: myapi
+ * mediaType: application/json
+ * baseUri: /api
+ * /base:
+ * /{id}:
+ * get:
+ * <p>
+ * OUTPUT:
+ * {@literal @}GetMapping("{id}")
+ *
+ * @author yura.nosenko
+ * @since 0.10.12
+ */
+public class SpringShortcutMappingMethodAnnotationRule implements Rule<JMethod, JAnnotationUse, ApiActionMetadata> {
+
+    @Override
+    public JAnnotationUse apply(ApiActionMetadata endpointMetadata, JMethod generatableType) {
+        JAnnotationUse requestMappingAnnotation;
+        switch (RequestMethod.valueOf(endpointMetadata.getActionType().name())) {
+            case GET:
+                requestMappingAnnotation = generatableType.annotate(GetMapping.class);
+                break;
+            case POST:
+                requestMappingAnnotation = generatableType.annotate(PostMapping.class);
+                break;
+            case PUT:
+                requestMappingAnnotation = generatableType.annotate(PutMapping.class);
+                break;
+            case PATCH:
+                requestMappingAnnotation = generatableType.annotate(PatchMapping.class);
+                break;
+            case DELETE:
+                requestMappingAnnotation = generatableType.annotate(DeleteMapping.class);
+                break;
+            default:
+                requestMappingAnnotation = generatableType.annotate(RequestMapping.class);
+                requestMappingAnnotation.param("method", RequestMethod.valueOf(endpointMetadata.getActionType().name()));
+        }
+
+        if (StringUtils.isNotBlank(endpointMetadata.getUrl())) {
+            requestMappingAnnotation.param("value", endpointMetadata.getUrl());
+        }
+        return requestMappingAnnotation;
+    }
+
+}
